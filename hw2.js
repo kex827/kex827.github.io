986820114f0d7ff8d313e4c8628d40fbb4a6f731
kex827 <kyle.esecson@kellogg.northwestern.edu>
2018-02-12 20:21:07 -0600

    Add files via upload

---
+++ hw2.js
@@ -0,0 +1,45 @@
+// To execute this code, be sure you're in the right directory;
+// (Mac)       cd ~/code/hw2
+// (Windows)   cd c:\code\hw2
+// Then type:
+// node hw2.js
+
+// HOMEWORK 2
+
+// Cryptocurrency is all the rage. You have decided to create your own,
+// KelloggCoin, that will massively disrupt financial markets at the Global
+// Hub.
+
+let Kyle = {
+  UserName: "Kyle"
+}
+
+let blockchain = [
+  { fromUser: null, toUser: "brian", amount: 51000 },
+  { fromUser: "brian", toUser: "kyle", amount: 39000 },
+  { fromUser: "brian", toUser: "jeff", amount: 7000 },
+  { fromUser: "Kyle", toUser: "jeff", amount: 400 },
+  { fromUser: "brian", toUser: "jeff", amount: 1500 },
+  { fromUser: "jeff", toUser: "brian", amount: 4500 },
+  { fromUser: "jeff", toUser: "Kyle", amount: 1750 }
+]
+
+let (UserName) = {
+  console.log("Brian's KelloggCoin balance is " + getBalance("brian"));
+  console.log("Kyle's KelloggCoin balance is " + getBalance("Kyle"));
+  console.log("Jeff's KelloggCoin balance is " + getBalance("jeff"));
+}
+
+// Write a function that, when given a username (i.e. brian, ben, or jeff),
+// returns the number of KelloggCoin that user has in their KelloggCoin "wallet".
+
+// HINT! You will have to use all the JavaScript programming fundamentals we've
+// learned about so far, i.e. variables, arrays, objects, conditionals, loops,
+// and functions, to complete this task. There are ways to do this using some
+// more advanced array functions, and by all means, experiment with this, but
+// get it done using just the basics first.
+
+// It should print out:
+// Brian's KelloggCoin balance is 8000
+// Ben's KelloggCoin balance is 10350
+// Jeff's KelloggCoin balance is 2650
